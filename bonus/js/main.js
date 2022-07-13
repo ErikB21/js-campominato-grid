4fd808c7ea382b0f46058dc4115d08c8da1012e1
ErikBorgogno <erikborgo.20@gmail.com>
2022-07-13 23:44:39 +0200

    change event click

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -1,16 +1,79 @@
 // prendo l'elemento principale che rappresenta la griglia
 const gridDom = document.getElementById('grid');
 
+// creo un singolo quadrato con al suo interno i numeri da 1 a quadratini(in base alla scelta varia)
+// aggiungo l'evento click e i suoi effetti
+// aggiungo il tutto alla griglia
+function creaGriglia(quadratini, livello){
+    for(let i = 1; i <= quadratini; i++){
 
+        const currentElement = createGridSquare(i, livello);
+
+        currentElement.addEventListener('click', 
+            function() {
+                this.classList.toggle('clicked');
+                console.log(currentElement.textContent);
+            }
+        );
+            gridDom.append(currentElement);
+    }
+}
+
+//genero una costante per il button genera
 const genera = document.getElementById('genera');
+//creo un evento che al click
+//richiamo la funzione creaGriglia con valori diversi
+//se l'elemento con id "difficoltà" è su easy darà 100 quadratini
+//se invece l'elemento con id "difficoltà" è su medium darà 81 quadratini
+//se l'elemento con id "difficoltà" è su hard darà 49 quadratini
 genera.addEventListener('click', 
     function(){
+        
         if(document.getElementById('difficoltà').value == 'easy'){
-            let riga = 10;
-            let colonna = 10;
-            let totalCells = colonna * riga;
-            
-            for(let i = 1; i <= totalCells; i++){
+            creaGriglia(100, 'easy');
+        }else if(document.getElementById('difficoltà').value == 'medium'){
+            creaGriglia(81, 'medium');
+        }else if (document.getElementById('difficoltà').value == 'hard'){
+            creaGriglia(49, 'hard');
+        }
+        
+        
+    }
+);
+
+
+//creo un elemento del dom di tipo div con classe square
+function createGridSquare(number, livello){
+
+    const currentElement = document.createElement('div');
+    currentElement.classList.add('square');
+    currentElement.classList.add(livello);
+    currentElement.append(number);
+    
+    
+    return currentElement;
+        
+}
+
+//creo un evento che al click elimina il gioco per poter iniziare una nuova partita
+const elimina = document.getElementById('elimina');
+elimina.addEventListener('click', 
+    function(){
+        gridDom.innerHTML = '';
+    }
+);
+
+
+/////////////////////////////////////////////////////////
+//Prima variante per l'evento del click
+//e il cambio della griglia a seconda della difficoltà
+//ma troppo ripetitiva e lunga
+
+
+/*genera.addEventListener('click', 
+    function(){
+        if(document.getElementById('difficoltà').value == 'easy'){
+            for(let i = 1; i <= 100; i++){
 
                 // creo un singolo quadrato con al suo interno i numeri da 1 a 100
                 const currentElement = createGridSquare(i);
@@ -27,11 +90,8 @@ genera.addEventListener('click',
                     gridDom.append(currentElement);
             }
         } else if(document.getElementById('difficoltà').value == 'medium'){
-            let riga = 9;
-            let colonna = 9;
-            let totalCells = colonna * riga;
             
-            for(let i = 1; i <= totalCells; i++){
+            for(let i = 1; i <= 81; i++){
 
                 // creo un singolo quadrato con al suo interno i numeri da 1 a 100
                 const currentElement = createGridSquare(i);
@@ -48,11 +108,7 @@ genera.addEventListener('click',
                     gridDom.append(currentElement);
             }
         }else if(document.getElementById('difficoltà').value == 'hard'){
-            let riga = 7;
-            let colonna = 7;
-            let totalCells = colonna * riga;
-            
-            for(let i = 1; i <= totalCells; i++){
+            for(let i = 1; i <= 49; i++){
 
                 // creo un singolo quadrato con al suo interno i numeri da 1 a 100
                 const currentElement = createGridSquare(i);
@@ -71,28 +127,4 @@ genera.addEventListener('click',
         }
         
     }
-);
-
-const elimina = document.getElementById('elimina');
-elimina.addEventListener('click', 
-    function(){
-        gridDom.innerHTML = '';
-    }
-);
-
-
-
-
-
-
-function createGridSquare(number){
-
-//creo un elemento del dom di tipo div con classe square
-    const currentElement = document.createElement('div');
-    currentElement.classList.add('square');
-    currentElement.append(number);
-
-// infine ritorno il div con classe square e relativo figlio square-number
-    return currentElement;
-    
-}
+);*/
